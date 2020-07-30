3a9cc5447c2f523d3a2aefd31d7470271feb8aa9
stewart <npcuong.011308@gmail.com>
2020-07-30 21:39:33 +0700

    Add grunt concat task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,2 +1,15 @@
-grunt.initConfig({
-});
+module.exports = function (grunt) {
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['style/*.css'],
+        dest: 'style/index.css',
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+}
